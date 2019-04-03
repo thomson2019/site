1020c05ac5fee9c5fd7f9251b47be1d043150700
Zapata Tinoco, Juan <Juan.Zapata@news.com.au>
2019-04-03 14:56:42 +1100

    adding display of stories in homepage and article page

--- theme/index.php
+++ theme/index.php
@@ -1,5 +1,16 @@
 <?php get_header(); ?>
 <div class="col-8">
-	here stories
+<?php
+if ( have_posts() ) {
+	// Load posts loop.
+	while ( have_posts() ) {
+		the_post();
+		get_template_part( 'template-parts/content/content' );
+	}
+} else {
+	// If no content, include the "No posts found" template.
+	?> No content to display <?php
+}
+?>
 </div>
 <?php get_footer(); ?>
